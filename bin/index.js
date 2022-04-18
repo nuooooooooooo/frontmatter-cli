321c702eea454d0b46a35a5d20bf386409d7abdb
web-Nuo <mlocht@outlook.com>
2022-04-18 21:03:37 +0200

    It works - partially

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,24 @@
 #! /usr/bin/env node
-console.log('The CLI is working')
+const fs = require('fs');
+const filePath = './test2.md';
+const frontMatter = `---
+setup: |
+  import xxx from '../../layouts/xxx.astro'
+  import xxx from '../../components/xxx.astro'
+title: My post title
+date: ${new Date().toLocaleDateString('en-us', { weekday:"short", year:"numeric", month:"long", day:"numeric"}) }
+author: Your Name
+tags: ['first','second']
+description: This is my blogpost description
+---
+# My post title
+`
+console.log(frontMatter)
+
+fs.writeFile(filePath,frontMatter,{ flag: 'ax' },  (err) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+})
+
