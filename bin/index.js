e66a9c68d6a5098eca6a2961f9e2853690a7fad6
web-Nuo <mlocht@outlook.com>
2022-04-18 22:47:22 +0200

    Format document

--- bin/index.js
+++ bin/index.js
@@ -7,22 +7,20 @@ let postTitle = "default name";
 const inquirer = require("inquirer");
 
 const questions = [
-//   {
-//     type: "input",
-//     name: "author",
-//     message: "Author name: ",
-//     default() {
-//       return "James Smith";
-//     },
-//   },
+  //   {
+  //     type: "input",
+  //     name: "author",
+  //     message: "Author name: ",
+  //     default() {
+  //       return "James Smith";
+  //     },
+  //   },
   {
     type: "input",
     name: "postTitle",
     message: "Blog post name: ",
     validate(value) {
-      const pass = value.match(
-        /^[a-z0-9A-Z\s]*$/g
-      );
+      const pass = value.match(/^[a-z0-9A-Z\s]*$/g);
       if (pass) {
         return true;
       }
@@ -32,41 +30,39 @@ const questions = [
   },
 ];
 
+inquirer.prompt(questions[i]).then((answers) => {
+  postTitle = answers.postTitle;
+  filePath = `./${postTitle.split(" ").join("-")}.md`;
+  postTitle = postTitle
+    .split(" ")
+    .filter((x) => x.length > 0)
+    .map((x) => x.charAt(0).toUpperCase() + x.slice(1))
+    .join(" ");
 
-inquirer
-  .prompt(questions[i])
-  .then((answers) => {
-    postTitle = answers.postTitle;
-    filePath = `./${postTitle.split(" ").join("-")}.md`;
-    postTitle = postTitle
-      .split(" ")
-      .filter((x) => x.length > 0)
-      .map((x) => x.charAt(0).toUpperCase() + x.slice(1))
-      .join(" ");
-
-    let frontMatter = `---
+  let frontMatter = `---
 setup: |
 import xxx from '../../layouts/xxx.astro'
 import xxx from '../../components/xxx.astro'
 title: ${postTitle}
 date: ${new Date().toLocaleDateString("en-us", {
-      weekday: "short",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    })}
+    weekday: "short",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })}
 author: Your Name
 tags: ['first','second']
 description: This is my blogpost description
 ---
 # ${postTitle}
+This is where the content goes
 `;
 
-    fs.writeFile(filePath, frontMatter, { flag: "ax" }, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-    });
-    console.log("created " + filePath);
+  fs.writeFile(filePath, frontMatter, { flag: "ax" }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
   });
+  console.log("created " + filePath);
+});
