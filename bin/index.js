acb3995e4bcf66b7be9c29b1b03876cbe71c0f7a
web-Nuo <mlocht@outlook.com>
2022-04-23 13:21:45 +0200

    save user settings to settings.json

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
 const fs = require("fs");
+const path = require("path")
 
 let filePath = "./default-name.md";
-let postTitle = "default name";
+let settingsFile = 'settings.json'
+settingsFile = path.join(__dirname,"utils", "settings.json")
 
 const inquirer = require("inquirer");
 const {
@@ -11,15 +13,12 @@ const {
   homescreenQuestions,
 } = require("./utils/questions.js");
 
-const {
-  defaultFrontMatter, 
-  buildFrontMatter
-} = require("./utils/frontmatter")
+const { defaultFrontMatter, buildFrontMatter } = require("./utils/frontmatter");
 
 inquirer.prompt(homescreenQuestions).then((answers) => {
   if (answers.chosenOption === "Create blog post") {
     inquirer.prompt(createPostQuestions).then((answers) => {
-      postTitle = answers.postTitle;
+      let postTitle = answers.postTitle;
       filePath = `./${postTitle.split(" ").join("-")}.md`;
       postTitle = postTitle
         .split(" ")
@@ -27,19 +26,32 @@ inquirer.prompt(homescreenQuestions).then((answers) => {
         .map((x) => x.charAt(0).toUpperCase() + x.slice(1))
         .join(" ");
 
-      fs.writeFile(filePath, buildFrontMatter(["author", "layout", "date"],"test",postTitle), { flag: "ax" }, (err) => {
+      fs.writeFile(
+        filePath,
+        buildFrontMatter(["author", "layout", "date"], "test", postTitle),
+        { flag: "ax" },
+        (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("created " + filePath);
+          }
+        }
+      );
+    });
+  } else {
+    inquirer.prompt(settingQuestions).then((answers) => {
+      //buildFrontMatter(answers.frontmatterElements, answers.author)
+      const settings = JSON.stringify(answers, null, "  ");
+      console.log(settings)
+      fs.writeFile(settingsFile, settings, { flag: "w" }, (err) => {
         if (err) {
           console.error(err);
         } else {
           console.log("created " + filePath);
         }
       });
-    });
-  } else {
-    inquirer.prompt(settingQuestions).then((answers) => {
-      //buildFrontMatter(answers.frontmatterElements, answers.author)
-      const settings = JSON.stringify(answers, null, '  ');
-      console.log(settings)
+      
     });
   }
 });
